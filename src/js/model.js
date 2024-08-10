d5b162531a4175141eb1890a253a254de487b614
Md Sarfaraz Alam <sarfarazstark@gmail.com>
2024-08-11 05:25:02 +0530

    chore: Update npm dependencies and add new package 'fractional' - Refactor the code with mvc model

---
+++ src/js/model.js
@@ -0,0 +1,32 @@
+import { async } from 'regenerator-runtime';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(state.recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
